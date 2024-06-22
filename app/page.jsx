5afad289e3850c2737815427b2f2417380a46494
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-06-22 15:40:23 -0700

    added the getNotes

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,21 @@
 "use client"
 
-import { useState } from "react";
+import { supabase } from "@lib/supabaseClient";
+import { useEffect, useState } from "react";
 
 export default function Home() {
 
   const[text, setText] = useState("")
+  const[todos, setTodos] = useState([])
 
+  useEffect(() => {
+    getTodo()
+  },[])
+
+  const getTodo = async () => {
+    const{data,error} = await supabase.from("notes").select()
+    console.log(JSON.stringify(data, null, 2))
+  }
   const handleSubmit = () => {
 
   }
@@ -15,8 +25,13 @@ export default function Home() {
         <div className="flex flex-col">
           <h1>Using Supabase</h1>
           <div className="flex gap-3">
-            <input  className="bg-gray-300 rounded-md px-2 py-[2px]" type="text" />
-            <button onClick={handleSubmit} className="bg-blue-500 text-white hover:bg-transparent hover:text-blue-500 px-2 py-1 rounded-md">Add</button>
+            <input onChange={(e) => setText(e.target.value)} className="bg-gray-300 rounded-md px-2 py-[2px]" type="text" />
+            <button className="bg-blue-500 text-white hover:bg-transparent hover:text-blue-500 px-2 py-1 rounded-md">Add</button>
+          </div>
+          <div className="overflow-y-scroll max-h-24 w-full">
+            {todos.map(todo => (
+              <li>{}</li>
+            ))}
           </div>
         </div>
       </div>
